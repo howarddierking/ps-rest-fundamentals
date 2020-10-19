123707fb24f28391c485ac577bc67c38fe369242
Howard Dierking <howard.dierking@sap.com>
2020-10-18 22:03:44 -0500

    WIP

--- demo/client/src/index.js
+++ demo/client/src/index.js
@@ -26,6 +26,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 
 const indexResourceId = 'http://localhost:8080/index.json';
 
@@ -51,7 +53,12 @@ const useStyles = makeStyles((theme) => ({
     },
     filterSelector: {
         minWidth: 120,
-    }
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 }));
 
 function BugsTable(props){
@@ -83,11 +90,12 @@ function BugsTable(props){
     )
 }
 
-function BugsFilters(props){
+function StatusSelector(props){
     const classes = useStyles();
     
     function handleStatusChange(e){
-        props.onFilterChange({status: e.target.value});
+        if(props.onSelectorChange)
+            props.onSelectorChange({status: e.target.value});
     }
 
     return (
@@ -98,6 +106,28 @@ function BugsFilters(props){
                 R.map(sf => (
                     <MenuItem key={sf.id} value={sf.id}>{sf.title}</MenuItem>
                 ), props.statusFilters)
+            }
+            </Select>
+        </FormControl>
+    );
+}
+
+function AssigneeSelector(props){
+    const classes = useStyles();
+    
+    function handleStatusChange(e){
+        if(props.onSelectorChange)
+            props.onSelectorChange({assignee: e.target.value});
+    }
+
+    return (
+        <FormControl className={classes.margin}>
+            <InputLabel>Assign To</InputLabel>
+            <Select value={props.assignedTo} className={classes.filterSelector} onChange={handleStatusChange}>
+            {
+                R.map(sf => (
+                    <MenuItem key={sf.id} value={sf.id}>{sf.fullName}</MenuItem>
+                ), props.possibleAssignees)
             }
             </Select>
         </FormControl>
@@ -154,7 +184,7 @@ function BugsListView(props){
     } else {
         return (
             <Box>
-                <BugsFilters onFilterChange={handleFilterChange} activeFilterStatus={bugsList.activeFilterStatus} statusFilters={statusFilters}/>
+                <StatusSelector onSelectorChange={handleFilterChange} activeFilterStatus={bugsList.activeFilterStatus} statusFilters={statusFilters}/>
                 <BugsTable bugs={bugsList.items} />
                 <BugsTablePager onPage={handlePage} prev={bugsList.prevPage} next={bugsList.nextPage} />
             </Box>
@@ -162,9 +192,123 @@ function BugsListView(props){
     }
 }
 
+function BugsEditView(props){
+    const classes = useStyles();
+    const [editModeActive, setEditModeActive] = useState(false);
+    const [commentText, setCommentText] = useState('');
+    const [comments, setComments] = useState([]);
+    const [assignmentId, setAssignmentId] = useState('');
+    const [statusId, setStatusId] = useState('');
+
+    function handleAddSave(e){
+        setEditModeActive(true);
+    }
+
+    function handleAddComment(e){
+        const comment = {
+            addedOn: new Date(Date.now()),
+            commentText: commentText
+        }; 
+
+        const newComments = R.append(comment, comments);
+
+        setComments(newComments);
+        setCommentText('');
+    }
+
+    function handleCommentChange(e){
+        setCommentText(e.target.value);
+    }
+
+    function handleAssignmentChange(e){
+        setAssignmentId(e.assignee);
+    }
+
+    function handleStatusChange(e){
+        setStatusId(e.status);
+    }
+
+    return (
+        <Box>
+        <Button variant="contained" onClick={handleAddSave}>
+            { editModeActive ? "Save" : "Add New Bug" }
+        </Button>
+        <br/>
+
+
+        <Grid container style={{visibility: editModeActive ? 'visible' : 'hidden'}}>
+            <Grid item xs={12}>
+                <Paper className={classes.paper} elevation={0} >
+                    <TextField label="Title" fullWidth />
+                    <TextField label="Description" rows={5} fullWidth multiline />
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper} style={{textAlign: 'left'}} elevation={0}>
+                    <AssigneeSelector possibleAssignees={props.possibleAssignees} assignedTo={assignmentId} onSelectorChange={handleAssignmentChange} />
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper} style={{textAlign: 'left'}} elevation={0}>
+                    <StatusSelector statusFilters={props.statusFilters} activeFilterStatus={statusId} onSelectorChange={handleStatusChange} />
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper} style={{textAlign: 'left'}} elevation={0}>
+                    <TextField value={commentText} onChange={handleCommentChange} label="Comments" rows={5} fullWidth multiline />
+                    <Button onClick={handleAddComment}>Add Comment</Button>
+                    {
+                        R.map(c => (
+                            <Box>
+                                <Typography>{c.addedOn.toISOString()}</Typography>
+                                <Typography>{c.commentText}</Typography>
+                            </Box>
+                        ), comments)
+                    }                        
+                </Paper>
+            </Grid>
+        </Grid>
+        </Box>
+    )
+}
+
 function App (props){
     const classes = useStyles();
-//TODO: put status filters in state at the app level, since they'll be used in the edit dialog as well
+
+    
+    
+    const [editBugId, setEditBugId] = useState(''); // should be null?
+    
+
+
+
+    // note that id is a url here and it's an integer in the index resource
+    // DEMO:  change the int to a URL without breaking the client
+
+    // TODO: change created by user to reflect the object definition rather than literal
+    const fakeBug = {
+        id: 'http://localhost:8080/bug/e04de755-7b1e-43d5-8857-bcf5faea9dba.json',
+        title: 'first bug',
+        description: 'lorem ipsum',
+        createdBy: {
+            id: 'http://localhost:8080/user/debf8087-b65f-4135-83d3-2803bd0f8848.json',
+            fullName: 'Howard Dierking'
+        },
+        createdOn: '2014-01-01T23:28:56.782Z',
+        modifiedOn: '2014-01-01T23:28:56.782Z',
+        status: {
+            id: 'open'
+        },
+        comments: [
+            {
+                addedOn: '2014-01-01T23:28:56.782Z',
+                commentText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        ]
+    }
+
+    
+
     return (
         <Container>
             <AppBar>
@@ -181,6 +325,9 @@ function App (props){
 
             <BugsListView statusFilters={props.apiIndex.statusFilters} bugsList={props.apiIndex.bugsList} />
             
+            <BugsEditView statusFilters={props.apiIndex.statusFilters} possibleAssignees={props.apiIndex.possibleAssignees} />
+
+
         </Container>
     );
 }
